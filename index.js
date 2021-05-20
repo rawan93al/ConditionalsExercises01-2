fa22a8688bc38956f78627c2bf1a43e606871f19
RawanAlmasalmeh <>
2021-05-20 21:09:52 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+let engineIndicatorLight = false;
+
+if (engineIndicatorLight === "green") {
+   console.log("engines have started");
+} else if (engineIndicatorLight === "green blinking") {
+   console.log("engines are preparing to start");
+} else {
+   console.log("engines are off");
+}
